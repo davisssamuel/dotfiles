6e47f172702902e1ae1c1783750aca8e4d169558
davisssamuel <davissamuelg1@gmail.com>
2024-05-08 15:06:39 -0400

    added nvim config

---
+++ .config/nvim/lua/vim-opts.lua
@@ -0,0 +1,15 @@
+-- WINDOW
+vim.cmd("set number")
+vim.cmd("set nowrap")
+vim.cmd("set cursorline")
+
+-- TABS and SPACES
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set backspace=2")
+vim.cmd("set shiftround")
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<C-j>", ":ter<CR>", {})
