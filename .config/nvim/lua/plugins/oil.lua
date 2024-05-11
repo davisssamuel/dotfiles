44bab4634f45ce8dcdcf355d9f7fc346d3a94dda
davisssamuel <davissamuelg1@gmail.com>
2024-05-11 02:01:13 -0400

    updated indentation in oil.lua

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,11 +1,11 @@
 return {
-	"stevearc/oil.nvim",
-	opts = {},
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("oil").setup({
-			view_options = { show_hidden = true },
+  "stevearc/oil.nvim",
+  opts = {},
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      view_options = { show_hidden = true },
       delete_to_trash = true,
-		})
-	end,
+    })
+  end,
 }
