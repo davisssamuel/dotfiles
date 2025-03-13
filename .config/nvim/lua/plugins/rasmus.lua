4bcd948be22075994a3aca2f80630abad4de34df
Sam <davissamuelg1@gmail.com>
2025-03-13 09:38:19 -0400

    deleted unused configs

--- .config/nvim/lua/plugins/rasmus.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"kvrohit/rasmus.nvim",
-	priority = 1000,
-	opts = ...,
-	config = function()
-		vim.o.background = "dark" -- or "light" for light mode
-    vim.cmd.colorscheme("rasmus")
-	end,
-}
