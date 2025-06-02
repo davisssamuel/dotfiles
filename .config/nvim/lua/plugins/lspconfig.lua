8d249f328c0b6f5bdb26546014f0e320f0660f5f
davisssamuel <davissamuelg1@gmail.com>
2025-06-02 17:27:55 -0400

    Added auto install for treesitter

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,7 +1,6 @@
 return {
 	"neovim/nvim-lspconfig",
 	config = function()
-
 		-- Add LSP capabilities to blink for every language server
 		local capabilities = require("blink.cmp").get_lsp_capabilities()
 		vim.lsp.config("*", {
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,22 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = {
+				"c",
+				"go",
 				"lua",
-				"markdown",
-				"json",
 				"javascript",
+				"typescript",
+				"json",
+				"markdown",
 				"python",
+				"vim",
+				"vimdoc",
 			},
+			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
