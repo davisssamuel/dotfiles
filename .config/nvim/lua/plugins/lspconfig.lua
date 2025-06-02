5ae604f89b6a8378b21e0e68bfc667724a97ea2d
davisssamuel <davissamuelg1@gmail.com>
2025-06-02 13:24:51 -0400

    Added comments to lspconfig

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,12 +1,14 @@
 return {
 	"neovim/nvim-lspconfig",
 	config = function()
-		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
+		-- Add LSP capabilities to blink for every language server
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
 		vim.lsp.config("*", {
 			capabilities = capabilities,
 		})
 
+		-- Manually configure LSP command for tsserver
 		vim.lsp.config("tsserver", {
 			cmd = { "typescript-language-server", "--stdio" },
 		})
