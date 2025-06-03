2832f09e9d79b36530f249985f677cbf301a14f1
davisssamuel <davissamuelg1@gmail.com>
2025-06-03 14:28:56 -0400

    Removed icons column from oil buffers

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,6 @@
 return {
 	"stevearc/oil.nvim",
-	dependencies = "nvim-tree/nvim-web-devicons",
+	-- dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
 		require("oil").setup({
 			delete_to_trash = true,
@@ -9,6 +9,7 @@ return {
 			float = { border = "single" },
 			confirmation = { border = "single" },
 			progress = { border = "single" },
+			columns = {}, -- remove icons column
 		})
 	end
 }
