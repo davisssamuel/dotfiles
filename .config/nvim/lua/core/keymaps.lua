1d784b9fe47d7b6e89cd4063a6c86313c336f6c6
davisssamuel <davissamuelg1@gmail.com>
2025-06-05 19:49:33 -0400

    Tweaked neovim config

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,24 +1,24 @@
 vim.g.mapleader = " "
 local map = vim.keymap.set
 
--- Configure Oil keybinds
+-- Oil keybinds
 map("n", "-", "<CMD>Oil<CR>", {})
 
--- Configure LSP keybinds
+-- LSP keybinds
 map({ "n", "v" }, "<leader>f", vim.lsp.buf.format, {})
 map({ "n", "v" }, "<leader>r", vim.lsp.buf.rename, {})
 map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
--- Configure split window keybinds
+-- Indenting selections
+map("v", "<", "<gv", {})
+map("v", ">", ">gv", {})
+
+-- Moving selections
+map("v", "J", ":m '>+1<CR>gv=gv", {})
+map("v", "K", ":m '<-2<CR>gv=gv", {})
+
+-- Moving between split windows
 map({ "n", "v" }, "<C-h>", "<CMD>wincmd h<CR>", {})
 map({ "n", "v" }, "<C-j>", "<CMD>wincmd j<CR>", {})
 map({ "n", "v" }, "<C-k>", "<CMD>wincmd k<CR>", {})
 map({ "n", "v" }, "<C-l>", "<CMD>wincmd l<CR>", {})
-
--- Configure moving selection 
-map("v", "J", ":m '>+1<CR>gv=gv", {})
-map("v", "K", ":m '<-2<CR>gv=gv", {})
-
--- Configure indenting selection 
-map("v", "<", "<gv", {})
-map("v", ">", ">gv", {})
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -50,14 +50,14 @@ return {
 					enable = true,
 					set_jumps = true, -- whether to set jumps in the jumplist
 					goto_previous_start = {
-						["[f"] = { query = "@function.outer",	desc = "Previous function" },
-						["[c"] = { query = "@class.outer",		desc = "Previous class" },
-						["[p"] = { query = "@parameter.inner",	desc = "Previous parameter" },
+						["[f"] = { query = "@function.outer",	desc = "previous function" },
+						["[c"] = { query = "@class.outer",		desc = "previous class" },
+						["[p"] = { query = "@parameter.inner",	desc = "previous parameter" },
 					},
 					goto_next_start = {
-						["]f"] = { query = "@function.outer",	desc = "Next function" },
-						["]c"] = { query = "@class.outer",		desc = "Next class" },
-						["]p"] = { query = "@parameter.inner",	desc = "Next parameter" },
+						["]f"] = { query = "@function.outer",	desc = "next function" },
+						["]c"] = { query = "@class.outer",		desc = "next class" },
+						["]p"] = { query = "@parameter.inner",	desc = "next parameter" },
 					},
 				},
 				swap = {
